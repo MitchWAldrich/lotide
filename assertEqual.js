fff3f74134a7e2e6fe2fdc3dab403459cdb6876e
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-06-21 21:37:31 +0000

    Created assertEqual to test future code

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🔴🔴🔴Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("the same", "the same");
+assertEqual("the same", "or not");
+assertEqual(3, 3);
+assertEqual(4, 12);
