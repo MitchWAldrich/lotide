123b86d5445984faf2d1b5ee6648cc74af1644cf
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-06-23 03:39:09 +0000

    Created a function to compare Arrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+
+const eqArrays = function(a, b) {
+  let stringA = a.join(', ');
+  let stringB = b.join(', ');
+  if (stringA === stringB) {
+    return true;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["Fred", "Latoya"], ["Fred", "Latoya"]), true);
+assertEqual(eqArrays([null, 2, NaN], [null, 2, NaN]), true);
