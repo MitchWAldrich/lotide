60c18bedc1f82967fbe4b47a104ff48017e10716
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-06-28 13:57:39 +0000

    updated eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -8,16 +8,32 @@ const assertEqual = function(actual, expected) {
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
 const eqArrays = function(arr1, arr2) {
-  if (arr1 === undefined) return "arr1 is undefined";
-  if (arr2 === undefined) return "arr2 is undefined";
-  let isEqual =
-  arr1.length === arr2.length ?
-    arr1.every(function(element, index) {
-      return element === arr2[index];
-    }) : false;
-  return isEqual;
+  for (i = 0; i < arr1.length; i++) {
+    for (j = 0; j < arr2.length; j++) {
+      if (arr2[j] !== arr1[j]) {
+        return false;
+      }
+    }
+  }
+  return true
 };
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays(["Fred", "Latoya"], ["Fred", "Latoya"]), true);
 assertEqual(eqArrays([1, 2, "red"], [1, 2, "green"]), true);
+/*
+if (arr1 === undefined) return "arr1 is undefined";
+if (arr2 === undefined) return "arr2 is undefined";
+let isEqual =
+arr1.length === arr2.length ?
+  arr1.every(function(element, index) {
+    return element === arr2[index];
+  }) : false;
+return isEqual;
+*/
